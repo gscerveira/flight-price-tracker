10c52eddcf8ddf1c7dc4881ff6c762c39e944155
gscerveira <61796134+gscerveira@users.noreply.github.com>
2024-09-24 12:33:21 -0300

    Adding a dependency

--- app/__init__.py
+++ app/__init__.py
@@ -3,6 +3,7 @@ from config import Config
 from flask_cors import CORS
 from flask_migrate import Migrate
 from flask_sqlalchemy import SQLAlchemy
+from sqlalchemy_utils import database_exists, create_database
 
 app = Flask(__name__)
 
@@ -12,5 +13,12 @@ CORS(app)
 
 db = SQLAlchemy(app)
 migrate = Migrate(app, db)
+
+# Create the database if it doesn't exist
+if not database_exists(db.engine.url):
+    create_database(db.engine.url)
+    print("Database created.")
+else:
+    print("Database already exists.")
 
 from app import models
